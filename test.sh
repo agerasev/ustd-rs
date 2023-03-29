791af2d45530d58cc3263b8ba76cabdd316fa192
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-03-29 10:51:08 +0700

    Use upstream freertos-rust

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git submodule update --init && \
+git submodule init && \
 cd freertos-rust && git submodule update --init freertos-rust-examples/FreeRTOS-Kernel && cd .. && \
 cargo test --no-default-features --features=backend-std && \
 cargo run --no-default-features --features=test-freertos && \

9b2be86944af36c069a548f83bcca5fa623f1191
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-08-16 11:18:32 +0700

    Produce compile error on misconfiguration

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 #![no_std]
 
+#[cfg(not(any(feature = "backend-std", feature = "backend-freertos")))]
+compile_error!("A backend must be selected");
+
+#[cfg(all(feature = "backend-std", feature = "backend-freertos"))]
+compile_error!("Only one backend at a time allowed");
+
 #[cfg(feature = "backend-freertos")]
 pub use freertos;
 

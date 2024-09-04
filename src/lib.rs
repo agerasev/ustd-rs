4b4e6ba70333e90e38ee02177957439c53468b2c
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-09-04 13:02:25 +0700

    Use FreeRTOS backend if selected both

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 #[cfg(not(any(feature = "std", feature = "freertos")))]
 compile_error!("A backend must be selected");
 
-#[cfg(all(feature = "std", feature = "freertos"))]
-compile_error!("Only one backend at a time allowed");
+//#[cfg(all(feature = "std", feature = "freertos"))]
+//compile_error!("Only one backend at a time allowed");
 
-#[cfg(feature = "std")]
+#[cfg(all(feature = "std", not(feature = "freertos")))]
 pub use backend_std::*;
 
 #[cfg(feature = "freertos")]

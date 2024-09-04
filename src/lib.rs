7727f88bb6eea225d8c5e0c27bb72353039d0043
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-09-04 14:05:41 +0700

    Use std by default

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
 #![no_std]
 
-#[cfg(not(any(feature = "std", feature = "freertos")))]
-compile_error!("A backend must be selected");
-
-//#[cfg(all(feature = "std", feature = "freertos"))]
-//compile_error!("Only one backend at a time allowed");
-
-#[cfg(all(feature = "std", not(feature = "freertos")))]
+#[cfg(not(feature = "freertos"))]
 pub use backend_std::*;
 
 #[cfg(feature = "freertos")]
